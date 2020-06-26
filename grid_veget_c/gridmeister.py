d00c59c9f2d8c3a5cadfed3ff2272c9355f41221
tonybutzer <tonybutzer@gmail.com>
2020-06-26 19:11:06 +0000

    automatic git update from Makefile

--- grid_veget_c/gridmeister.py
+++ grid_veget_c/gridmeister.py
@@ -1,3 +1,73 @@
+import os
+import json
+
+def _write_geojson(filename, coord_list):
+    print(coord_list)
+    polycs = []
+    polycs.append(coord_list)
+    geos = []
+    for polyc in polycs:
+        poly = {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [polyc]
+            }
+        }
+        geos.append(poly)
+
+    geometries = {
+       'type': 'FeatureCollection',
+        'features': geos,
+    }
+
+    #geo_str = json.dumps(geometries)
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(geometries, f, ensure_ascii=False, indent=4)
+    print('WROTE filename {}'.format(filename))
+
+def _write_shp(geojson_filename):
+    shp_filename = geojson_filename.split('.json')[0] + '.shp'
+    print(geojson_filename,shp_filename)
+    cmd='ogr2ogr -f \"ESRI Shapefile\" {} {}'.format(shp_filename, geojson_filename)
+    os.system(cmd)
+
+
+
+def _make_chip_poly(ul_lat, ul_lon, increment):
+    coord_list = []
+    ul_lat = int(ul_lat)
+    ul_lon = int(ul_lon)
+    ul_lon_lat = [ul_lon, ul_lat]
+    ur_lon_lat = [ul_lon + increment, ul_lat]
+    lr_lon_lat = [ul_lon + increment, ul_lat - increment]
+    ll_lon_lat = [ul_lon, ul_lat - increment]
+    print (ul_lon_lat)
+    coord_list.append(ul_lon_lat)
+    print (ur_lon_lat)
+    coord_list.append(ur_lon_lat)
+    print (lr_lon_lat)
+    coord_list.append(lr_lon_lat)
+    print (ll_lon_lat)
+    coord_list.append(ll_lon_lat)
+    print (ul_lon_lat)
+    coord_list.append(ul_lon_lat)
+    return(coord_list)
+
+
+def _parse_chip_name(chip_name):
+    print (chip_name)
+    tn = chip_name.split('chip')[-1]
+    print(tn)
+    ul_lat = tn.split('N')[0]
+    tn = tn.split('N')[-1]
+    print(tn)
+    print(ul_lat)
+    ul_lon = tn.split('E')[0]
+    print(ul_lon)
+    return ul_lat, ul_lon
+
 
 
 def _parse_tile_name(tile_name):
@@ -13,6 +83,11 @@ def _parse_tile_name(tile_name):
     return ul_lat, ul_lon
 
 
+def make_filename(tile_name, chip_name, extension):
+    filename = tile_name + '_' + chip_name + extension
+    return(filename)
+
+
 class GridMeister:
     """
     This class partitions a 10 degree tile into 2 degree chips
@@ -20,6 +95,7 @@ class GridMeister:
 
     def __init__(self, tile_name):
 
+        self.tile_name = tile_name
         self.tile_increment = 10
         self.chip_increment = 2
 
@@ -62,3 +138,15 @@ class GridMeister:
 
         return CHIP_LIST
 
+    def create_chip_shp(self, chip_name):
+        self.aoi_dir = './AOI'
+        print(chip_name)
+        ul_lat,ul_lon = _parse_chip_name(chip_name)
+        print(ul_lat,ul_lon)
+        coord_list = _make_chip_poly(ul_lat,ul_lon, self.chip_increment)
+        print(coord_list)
+        filename = make_filename(self.tile_name, chip_name, '.json')
+        print(filename)
+        full_filename =  self.aoi_dir = './AOI' + '/' + filename
+        _write_geojson(full_filename, coord_list)
+        _write_shp(full_filename)
--- grid_veget_c/run_one_10degree_square.py
+++ grid_veget_c/run_one_10degree_square.py
@@ -5,3 +5,8 @@ gm = GridMeister('tile40N-80E')
 chip_list = gm.chip_list()
 
 print(chip_list)
+
+
+for chip in chip_list:
+        gm.create_chip_shp(chip)
+
